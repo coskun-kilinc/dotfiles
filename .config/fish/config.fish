1f1f5a69c49bdd9d528c965f6ab20cf2ffd8eff4
Coskun Kilinc <82747562+coskun-kilinc@users.noreply.github.com>
2024-04-08 12:23:36 +1000

    update aliases in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,14 @@
-# set fish_greeting (set_color green)"
-#  ********   **    ********   **      **
-# /**/////   /**   **//////   /**     /**
-# /**        /**  /**         /**     /**
-# /*******   /**  /*********  /**********
-# /**////    /**  ////////**  /**//////**
-# /**        /**         /**  /**     /**
-# /**        /**   ********   /**     /**
-# //         //   ////////    //      // 
-# "
-set fish_greeting ""
+set fish_greeting (set_color green)"
+ ********   **    ********   **      **
+/**/////   /**   **//////   /**     /**
+/**        /**  /**         /**     /**
+/*******   /**  /*********  /**********
+/**////    /**  ////////**  /**//////**
+/**        /**         /**  /**     /**
+/**        /**   ********   /**     /**
+//         //   ////////    //      // 
+"
+# set fish_greeting ""
 
 set -gx TERM xterm-256color
 
@@ -28,28 +28,30 @@ alias la "ls -A"
 alias ll "exa -l --group-directories-first --git --icons"
 alias lla "exa -la --group-directories-first --git --icons"
 alias g git
+alias lg lazygit
 
-
+# >>> functions >>>
 alias html-to-md "pbpaste | pandoc -f html -t markdown | pbcopy"
 
+# >>> editor >>>
 set -gx EDITOR nvim
 
+# >>> path >>>
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	# >>> conda initialize >>>
+	# !! Contents within this block are managed by 'conda init' !!
+	eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+	# <<< conda initialize <<<
 end
 
 # >>> add brew to path >>>
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
